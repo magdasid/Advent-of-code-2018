ec42484596b894091a787ff2916a99041f4e839b
Magda Sidor <magda_si@wp.pl>
2018-12-02 15:21:16 +0100

    Add day 2

---
+++ day 2/main.js
@@ -0,0 +1,298 @@
+let input = `ubkfmdjxyzlbgkrotcepvswaqx
+uikfmdkuyzlbgerotcepvswaqh
+uikfmdpxyzlbgnrotcepvswoeh
+nikfmdjxyzlbgnrotqepvswyqh
+qgkfmdjxyzlbgnmotcepvswaqh
+uikfmdjxyzqbgnrytcepvsbaqh
+uikfmdjxyzibgprotcecvswaqh
+uikfmajxyzlcgnrojcepvswaqh
+uvkfsdjxyzlbgnrotcepvjwaqh
+uikfmdjxfzlbggrotcepvswawh
+uikfmhjxyzlbgnuotcepvjwaqh
+uikfmdjxyzlbuzcotcepvswaqh
+uikfmdwxyzlbgnrotcepvfwamh
+uikfmdexyzlbgnroecepvswhqh
+uikfmdjuyzlbgnrotcqpvswafh
+uikfddjxyzvbgnrotceppswaqh
+yikfwdjxyzlbgnrotcepvswagh
+uiktmdjxyzlbgnrotceposwajh
+uikfmdsxyzlbgnroteetvswaqh
+uikfpdjxyzlbgnroncepvswuqh
+uikfmtjxyzlbgurotcepvswaoh
+eikfmdjxyjlbgnrotcepyswaqh
+uikfkdjxyzlbgnrotcepvszaqv
+uikfrdjxtwlbgnrotcepvswaqh
+uikfmdjxyzlbgnrotpepwswahh
+kikfmdjxyzlbgnkotcepvswqqh
+uikfkduxyzlbgnrotcepvswafh
+uikfxhjxyzlbgnrotcegvswaqh
+uikfmdjxyzlmgnrotcenvawaqh
+uzkfmddxyzlbgnrltcepvswaqh
+uikfmdjxyzlbgnrobcepisqaqh
+uijfmdjxyzlbgnrotcexvrwaqh
+uiwjmdjxyzlbgnrotceprswaqh
+uhkqmdjxwzlbgnrotcepvswaqh
+uiktmsjxyzwbgnrotcepvswaqh
+uikfmdjxyztbgnqotcepvswcqh
+uibfmdjxyzlbgnroqcepvswaqx
+uwkfmdjxyxlbgnrotcfpvswaqh
+uikumdjxyzlbgnrstceposwaqh
+uikfzdjxyznhgnrotcepvswaqh
+uikuydjxyzlbgnrotqepvswaqh
+uikfmdqxyzlbgnrotfefvswaqh
+yikfmdjxyzlbrnrqtcepvswaqh
+uiifmdjxyzlbenrotcfpvswaqh
+uxkjmdjxyzlbgnrotcmpvswaqh
+uikgmdjxyzlbgnrotceovlwaqh
+uikfmdjxyzvbgzrotcenvswaqh
+uiufmdjxyzlbgnrotceposwazh
+uiafmdjxyzlignmotcepvswaqh
+uikfmdjxyzwbgnsotlepvswaqh
+uikjmdjvyzlbgnrotcenvswaqh
+uikfmdjxyzlbonroteepvswaqi
+uikfmdjxyzldgnroxcepviwaqh
+uikvmdjxyzlbgdrotrepvswaqh
+uikfmdjyyzwbgnrotcepvzwaqh
+uikfmdjxyzzbnnvotcepvswaqh
+uikomdjxyzlbgnrotcepvuwhqh
+uikfmmjxyzbbgnrotcepvswayh
+uikfmdjfezlbgprotcepvswaqh
+uzkfmojxmzlbgnrotcepvswaqh
+uipfmdjxyzlbgnrotceyuswaqh
+uikfmdjxyzlkgnmotcepvswadh
+uikfmdjxyzlbgnuctcbpvswaqh
+uikfqdjxyzlbogrotcepvswaqh
+uikfmdjxyzlfynrotcepvswash
+uikfmdjxizzmgnrotcepvswaqh
+uhkfmdjxyzlbhnrotcenvswaqh
+uipfmdjxyzlbgorotcepfswaqh
+uikfmdjxyznbgnrotcepvswfah
+uikfmujxyzlbgnrotnepvssaqh
+uikfmdjxyzlxgnrotcepvsrwqh
+uikfmdjxszlbgnrottvpvswaqh
+umkfmdskyzlbgnrotcepvswaqh
+uikfmdjxyzlbgorotcwpzswaqh
+uikfmdhxyzzbgnzotcepvswaqh
+jikfmdjxyzlbgnrotcepveyaqh
+uirfmdlxyzlbgnjotcepvswaqh
+uikfmdjxyzlbgnrorxepvswazh
+uikfmdjxyzltgnrotcepvsxaqi
+uikfmdjxyzlbxlrotcepkswaqh
+uvkfmdjxyzlbgnrotcopvswxqh
+uxkfmdjxkzlbgnrotcepvswqqh
+uikfmdjxyzlbqnrotcepvhwrqh
+uikfmdjxyzlvgnrolcepvswrqh
+urkfmdixyzlbgnrotcepvsjaqh
+uikfmdjxymlbsnrotcepvswcqh
+uikfmdjxyilbgnrotcepvcwzqh
+uikfadjxlzlbgnrotcepvswaql
+uikfmdjxuzlbgdrotcgpvswaqh
+yikfmdgxyzlbgnrotcepvswarh
+uikfmdjxyzlbgorotcepcswaqv
+uikkmdjxyzlbvnrotcepvvwaqh
+uwzfmdjxyxlbgnrotcfpvswaqh
+uikfmdjxyztbgnrotcrtvswaqh
+uiufmdjxyzhbgnrotcupvswaqh
+uikfzdjcyzlbgnrotcfpvswaqh
+uipfmdjxyzlbgnrotavpvswaqh
+uikfmajxyzlbgnrotcepbsxaqh
+uikfmdjfyzlbgnrotbepvswmqh
+gikkmdjxyzlbgnrptcepvswaqh
+uikfmdjxqvlbgnrotsepvswaqh
+fikfmdjxyzlbgnrotcegvswoqh
+idkfmdjxyzlbgnrotcepwswaqh
+uikfmdqxyzlbmnrobcepvswaqh
+uikfmdjxyzpbgnroicepvsyaqh
+uikfmkoxyzlbgnrotcgpvswaqh
+unkfmdjxyzlbpnrolcepvswaqh
+uikfmdjmyzlbgfrotcupvswaqh
+ujkfmdjxynlbgnroteepvswaqh
+uikfmljxyzlbgnaotcepvsiaqh
+uikfmdjdyzlbgnrotcepvtwaqd
+uikfmdjxyzlbgnyotcepimwaqh
+uikfmdjxyzfbjnrotcepvxwaqh
+eiwfmdjxyzlbgnrdtcepvswaqh
+umkhmdjxyzlbgnrotceivswaqh
+uikfmdjxyzlbgnrotcwpvswneh
+uckfmdjxyzlbknrotcepvswauh
+uiofmdjoyzlbgnrbtcepvswaqh
+uikfmdbxyzlbgnrotcepaslaqh
+uimfmdjxyalbgnrotcepvswaxh
+uqkfmdjxyzlbwnrotcepmswaqh
+uiyfmdjxyzlbgnrotcepvswxuh
+uikfmdjxyzlbgmrotgepvswamh
+uikfmdjxyqlbgarozcepvswaqh
+uikfmdjxyzabanpotcepvswaqh
+uikfmdjgyzlbsnrotcepvswaqj
+uikfmdjxyzlbwnrottepvsvaqh
+uikfmdjxyzlbbnrotcepvofaqh
+uikfudjxyzlbgnustcepvswaqh
+cskfmqjxyzlbgnrotcepvswaqh
+uiwfmddxyzlbgnrotccpvswaqh
+eikpmdjxyzlbgnrotcesvswaqh
+uikfmdzxyzlngnrrtcepvswaqh
+uikfmdjxyzlbgnrotcepaswtph
+uirfmdjxyzlbgnrotcepvswsqe
+uikjmdjxqzlbgirotcepvswaqh
+uikfmdjxsllbknrotcepvswaqh
+uikfmdjxyqlbgcrotcepvswaqu
+uikfmdjsymlbgnrotcebvswaqh
+uikfmdjxezlbgnroccepviwaqh
+uikfmdjiyzjbgnrotcepvswarh
+uqkfmdjxyzmbgnrotcepvslaqh
+unkfmdjxyzlbinrotceplswaqh
+uikfmdjxyzpbgnrjtcedvswaqh
+uicfmdjxyzlbgrrotcepvswamh
+ukknmdjxyzlbgnqotcepvswaqh
+uikfudjxyzlbdnrztcepvswaqh
+uikfmdjxyzlbgnrozcepvswawk
+uikfmduxyzsbgnrotcepvswauh
+uikfmdjxyzljbnrotcenvswaqh
+uukfmdjxyzlbgnrotcckvswaqh
+uilfldjxyzlbgnrotcdpvswaqh
+uckfmdjxyvybgnrotcepvswaqh
+qikfmdjxyglbgnrotcepvrwaqh
+uikfmhjxyzltgnrotcepvswbqh
+uikfmdjxipabgnrotcepvswaqh
+uikfmdjxyzlbgnrotceovswanl
+uikfmdjxyzlbgirotcapvswahh
+uikfucjxyflbgnrotcepvswaqh
+pikfmdjxyzpbgnrotcepvswaeh
+uikfmdjiyylbgnrotcervswaqh
+uikfmdjgyzlbgnrotcaevswaqh
+uikvmdjxyzlbunrotcepvswarh
+uikfadjuyzpbgnrotcepvswaqh
+uikfmdjxyzlbgnrotcepsawaqj
+eikfmdjxyflbgnrotcepvswaeh
+uisfmdaxyzlbgnrotcepvswlqh
+vikfmdjxyzlzgnrotcepvswanh
+ukkfmdoxyzlbgnrotcewvswaqh
+uikfmdhxyzlbgnrotcrpvbwaqh
+uhkfmdjwezlbgnrotcepvswaqh
+uikfddjxyzlbgnroteepvpwaqh
+uikfmdjxczlbgncotiepvswaqh
+uikfvdjxyzlbgnrotcnpvsaaqh
+uikfmdjxyzlbgnritcepvlwmqh
+uikfmdjxczlcgnrotcecvswaqh
+mikfmdjxyzlbgnrotcepvswasi
+uifvmdjxyzlbgnrotpepvswaqh
+uikzmdjxyzlbgnrotrepvswaqs
+uikfmqjqyzlbunrotcepvswaqh
+uikfpdyxyzlbgnrotcepvswagh
+uikfmdjxyzobgnrotrlpvswaqh
+zisdmdjxyzlbgnrotcepvswaqh
+uikfmdjxyzlbgnlotiesvswaqh
+uikfddixyzlbgnroucepvswaqh
+uijfmdrxyzlbgnrotoepvswaqh
+uikfmdcxbzlbgnrotcepvpwaqh
+uikfmdjxxzlbfnrotcecvswaqh
+upkfmdjxyzmtgnrotcepvswaqh
+uikfmdrxyzlbgnrjtcepvswaqp
+uizfmdjxyzlbsnrotcepviwaqh
+uidfmdjxyslbgnrotcxpvswaqh
+uikfmdjxyzlbgnrovyepvsdaqh
+uiafmdjxyzlbgnrxtcepvsdaqh
+ugkfmdjxyzlbgnrodcepvswawh
+pikfmtjxyzhbgnrotcepvswaqh
+uikfmdjxyzlfgnvotcepvswtqh
+uikbmdjxyzlbgerotcepvswaqm
+uikfmdjxyhlbdnrotcepvswaqy
+uikfgdjxyzlbgnhotcepvswdqh
+uikfmdpxyzlbgnrotcepvscanh
+uikfmdjxyzsbgnretcepvswaqn
+uikfddjxyzlrgnrotcepvsbaqh
+uikfmdjxyzlbgnrotcqnrswaqh
+uhkfmejxyzlbgnrotvepvswaqh
+uikimdjxyzlngnrotceprswaqh
+uikfmdjxyzwbunrotiepvswaqh
+rikfmdjxyzlbgnrotcypvssaqh
+uikfmdjxyzlbdnrotcrpvswsqh
+uekfmdjxkzlbznrotcepvswaqh
+uikfmdjxyglbgvrotcepvswaqv
+uikfmcjxyzlbgnrotmeovswaqh
+uikfmdjxyznbgnrozcepvswaqm
+uikfmdjxyzlbdnrotcepdsyaqh
+umkfmdjxfzlbgnrotiepvswaqh
+uitfmdjxyzvbcnrotcepvswaqh
+uikfmdjqyzlbgnrvtcepvlwaqh
+uikfmdjxyzkbworotcepvswaqh
+uikfmdzxyzlbwnrotcypvswaqh
+uikfmdjxyklbgnrftyepvswaqh
+uinfmsjxyzlbgnrotcepsswaqh
+xisfmdjxymlbgnrotcepvswaqh
+uikfmdjxjzlbgnropcepvswaqv
+uikfmdjxyalegyrotcepvswaqh
+uikfydjxyzlbgnrotcekvswtqh
+uikwmojxyzlbgnromcepvswaqh
+uikdmdjayzlbgnrotcepvswzqh
+uikfmdjxyzlmvnrotctpvswaqh
+uikfmbjxyzlbgnrotceptsweqh
+yvkfmdjxyzlbgqrotcepvswaqh
+uikomdjxfxlbgnrotcepvswaqh
+uikfmdjxczlbgnroocepgswaqh
+uikemdjxizlbgnrotcegvswaqh
+uikdmdjxyzlbgwrotceprswaqh
+tiyfmdjfyzlbgnrotcepvswaqh
+tmkfmdjxyzlbgirotcepvswaqh
+uikfmdjxyzebgnzotcepqswaqh
+uikfmljxyzlbgnrwtcepvswaeh
+uikfmojxyzlbgnrotcepbswwqh
+uikfmdjxyzlbgsrotcewvswwqh
+uikfmdjhyzlbgdrogcepvswaqh
+uikfmvjxyzlbrnrltcepvswaqh
+jikfmdjxyzlbgnrotcepvgcaqh
+uikhtdjxyzlbgnrotcepvswarh
+uikfmdjxyezbgnritcepvswaqh
+uikfwdjxyzlbgnrotzepvsnaqh
+uikfmdjxqylbgnrobcepvswaqh
+zikzmdjxyzlbgnrhtcepvswaqh
+uiksmzjxyzlbgnrftcepvswaqh
+uikfmdjxuzlbgnrotcepvsvaqc`;
+
+const convertData = (input) => {
+    let convertedData = input.toString().replace(/\n/g, ' ');
+    let array = convertedData.split(" ");
+    return array;
+}
+const findCheckSum = (input) => {
+    let array = convertData(input);
+    let occurance2 = 0;
+    let occurance3 = 0;
+    for(let i=0; i<array.length; i++) {
+        occurance2 += findOccurance(array[i], 2);
+        occurance3 += findOccurance(array[i], 3);
+    }
+    console.log(occurance2*occurance3);
+}
+const findOccurance = (arrayOfLetters, number) => {
+    let dictionary = {};
+    for (let i = 0; i < arrayOfLetters.length; i++) {
+        let letter = arrayOfLetters[i];
+        dictionary[letter] = dictionary[letter] != null ? dictionary[letter] + 1 : 1;
+    }
+
+    let letters = Object.keys(dictionary);
+    let occurance = letters.find(letter => dictionary[letter] === number) != null;
+    return occurance; 
+}
+const findCommonLetters = (input) => {
+    let array = convertData(input);
+    for(let i=0; i<array.length-1; i++) {
+        for(let j=i+1; j<array.length-1; j++) {
+            let common = 0;
+            let result = '';
+            for(let k=0; k<array[i].length; k++) {
+                if(array[i].charAt(k) === array[j].charAt(k)) {
+                    common += 1;
+                    result += array[i].charAt(k);
+                }
+            } 
+            if(common >= array[i].length-1) {
+                return result;
+            }
+        }
+    }
+}
+
+findCheckSum(input);
+console.log(findCommonLetters(input));
